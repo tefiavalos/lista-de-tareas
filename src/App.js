bc2af7e7903e112ef271f91a875ae698f8ebeec8
tefiavalos <emlavalos92@gmail.com>
2020-03-27 22:07:00 -0300

    tarea en clase

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const ElementoDeLaLista = ({ tarea, i }) => {
+    return (
+      <li className={tarea.completado ? "completada" : ""} key={i}>
+        {tarea.nombre}
+        <button onClick={() => handleEraseCLick(i)}>X</button>
+        <button onClick={() => handleCompleteClick(i)}>{tarea.completado === false ? `Marcar como completado` : `Marcar como no completado`}</button>
+      </li>)
+  }
+  //por que en el cod agregamos i? si antes funcionaba
+  const handleEraseCLick = indiceTareaABorrar => {
+    const tareasFiltradas = tareas.filter((tarea, i) => i !== indiceTareaABorrar)
+    setTareas(tareasFiltradas)
+  }
+
+  const handleCompleteClick = indiceTareaACompletar => {
+    const tareasModificadas = tareas.map((tarea, i) => {
+      tarea.completado = i === indiceTareaACompletar ? !tarea.completado : tarea.completado
+      return tarea
+    })
+    setTareas(tareasModificadas)
+  }
+
+  const [tareas, setTareas] = useState([
+    { nombre: "Hacer fiaca", completado: true },
+    { nombre: "Dormir", completado: true },
+    { nombre: "Descansar", completado: false },
+  ])
+
+  const [nuevaTarea, setNuevaTarea] = useState("")
+
+  const handleChange = e => {
+    setNuevaTarea(e.target.value)
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault()
+    const listaDeTareas = tareas
+    const objeto = {
+      nombre: nuevaTarea,
+      completado: false,
+    }
+    listaDeTareas.push(objeto)
+    setTareas(listaDeTareas)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {tareas.map((tarea, i) =>
+          <ElementoDeLaLista key={i} tarea={tarea} i={i}></ElementoDeLaLista>)}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Agregar tarea..." onChange={handleChange}></input>
+        <input type="submit" value="Agregar"></input>
+      </form>
     </div>
   );
 }
